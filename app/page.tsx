c8ba08d1e4dc1512b0297bb1769dfdb5211841ba
ammad amin <153178783+ammad-amin-1999313@users.noreply.github.com>
2025-05-21 17:35:16 +0500

    finance section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import BussinessData from "@/components/bussinessdata";
 import Navbar from "@/components/common/navbar";
 import Finance from "@/components/finance";
 import Hero from "@/components/home/hero";
+import Testimonials from "@/components/testimonials";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <BussinessData />
       <Finance />
+      <Testimonials />
     </div>
   );
 }
--- components/finance/index.tsx
+++ components/finance/index.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import CommonButton from "../common/navbar/button";
 import vector from "@/assets/svgs/Vector.svg";
 import Image from "next/image";
+import finance_icon from "@/assets/svgs/finace-phone.svg";
+import overlay_left from "@/assets/svgs/overlay-left.svg";
+import overlay_right from "@/assets/svgs/Overlay-right.svg";
+import overlayExtra from "@/assets/svgs/Overlay+.svg";
+import overlayShadow from "@/assets/svgs/Overlay+Shadow.svg";
 
 const Finance = () => {
   const handleButtonClick = () => {
@@ -10,7 +15,7 @@ const Finance = () => {
   };
 
   return (
-    <div className="bg-(--primary) py-24">
+    <div className="bg-(--primary) pt-24">
       <div className="w-[65%] mx-auto space-y-6 ">
         <h1 className="text-[var(--primarytext)] font-semibold text-5xl md:text-6xl text-center leading-tight font-display">
           Simplify your finances with our <br /> comprehensive toolkit
@@ -35,8 +40,25 @@ const Finance = () => {
         </div>
       </div>
       {/* images */}
-      <div className="w-[70%] mx-auto">
+      <div className="w-[70%] mx-auto relative">
         <Image src={vector} alt="vector" />
+        <div className=" absolute top-0 left-0 bottom-0 right-0">
+          <div className="flex items-center justify-center">
+            <Image src={finance_icon} alt="finace" width={300} height={600} />
+          </div>
+          <div className="absolute top-[90px] right-[105px]">
+            <Image src={overlay_right} alt="overlay" width={317} height={250} />
+          </div>
+          <div className="absolute top-[210px] left-[55px]">
+            <Image src={overlay_left} alt="overlay" width={280} height={200} />
+          </div>
+          <div className="absolute top-[113px] left-[178px]">
+            <Image src={overlayShadow} alt="overlay" width={277} height={278} />
+          </div>
+          <div className="absolute top-0 right-0">
+            <Image src={overlayExtra} alt="overlay" width={318} height={320} />
+          </div>
+        </div>
       </div>
     </div>
   );
