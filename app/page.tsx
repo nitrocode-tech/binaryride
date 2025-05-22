8b15e9b8b9003585f8bfe6f36d80cab70fdb555b
ammad amin <153178783+ammad-amin-1999313@users.noreply.github.com>
2025-05-22 18:35:37 +0500

    final

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import BussinessData from "@/components/bussinessdata";
 import Navbar from "@/components/common/navbar";
 import Finance from "@/components/finance";
 import Hero from "@/components/home/hero";
+import Pricing from "@/components/pricing";
 import Testimonials from "@/components/testimonials";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <BussinessData />
       <Finance />
       <Testimonials />
+      <Pricing />
     </div>
   );
 }
